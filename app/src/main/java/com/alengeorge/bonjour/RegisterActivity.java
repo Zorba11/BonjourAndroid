2416984f21eb090d5549d1afc5f692ab6a0c627b
zorba11 <zorbathegreek111@gmail.com>
2019-04-15 15:14:40 -0400

    Firebase authentication accomplished

--- app/src/main/java/com/alengeorge/bonjour/RegisterActivity.java
+++ app/src/main/java/com/alengeorge/bonjour/RegisterActivity.java
@@ -1,12 +1,22 @@
 package com.alengeorge.bonjour;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -14,10 +24,16 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText userEmail, userPassword;
     private TextView alreadyHaveAccountLink;
 
+    private FirebaseAuth auth;
+    private ProgressDialog loadingBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        FirebaseApp.initializeApp(this);
+        auth = FirebaseAuth.getInstance();
 
         InitializeFields();
 
@@ -30,6 +46,55 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                CreateNewAccount();
+            }
+        });
+
+    }
+
+    private void CreateNewAccount() {
+        String email = userEmail.getText().toString();
+        String password = userPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Please enter email",Toast.LENGTH_LONG).show();
+
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Please enter your password",Toast.LENGTH_LONG).show();
+
+        }
+        else{
+
+            loadingBar.setTitle("Creating new account");
+            loadingBar.setMessage("Please wait, while we are creating a new account for you...");
+            loadingBar.setCanceledOnTouchOutside(true);  //loading bar stays untill new account is created
+            loadingBar.show();
+
+            auth.createUserWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                SendUserToLoginActivity();
+                                Toast.makeText(RegisterActivity.this,"Account create successfully",Toast.LENGTH_LONG).show();
+                                loadingBar.dismiss();
+                            }
+                            else{
+
+                                String message = task.getException().toString(); //get the type of error occured
+                                Toast.makeText(RegisterActivity.this,"Error : "+message,Toast.LENGTH_LONG).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
+
     }
 
     private void InitializeFields() {
@@ -39,10 +104,14 @@ public class RegisterActivity extends AppCompatActivity {
         userPassword = findViewById(R.id.register_password);
         alreadyHaveAccountLink = findViewById(R.id.already_have_account_link);
 
+        loadingBar = new ProgressDialog(this);
+
     }
 
     private void SendUserToLoginActivity() {
         Intent loginIntent = new Intent(RegisterActivity.this,LoginActivity.class);
         startActivity(loginIntent);
     }
+
+
 }
