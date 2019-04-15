32eaa5bc5db2c1f0ce5fc01d60d47c44430d12a2
zorba11 <zorbathegreek111@gmail.com>
2019-04-14 22:03:37 -0400

    Chats,Contacts,Groups fragment classes added.TabsAccessorAdaptor class also created

--- app/src/main/java/com/alengeorge/bonjour/MainActivity.java
+++ app/src/main/java/com/alengeorge/bonjour/MainActivity.java
@@ -1,13 +1,37 @@
 package com.alengeorge.bonjour;
 
+import android.annotation.SuppressLint;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolBar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+
+    private TabsAccessorAdapter myTabsAccessorAdapter;
+
+
+    @SuppressLint({"RestrictedApi", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolBar = findViewById(R.id.mainPageToolBar);
+        setSupportActionBar(toolBar);
+        getSupportActionBar().setWindowTitle("Bonjour");
+
+        myViewPager = findViewById(R.id.mainTabs_pager);
+        myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsAccessorAdapter);
+
+        myTabLayout = findViewById(R.id.mainTabs);
+        myTabLayout.setupWithViewPager(myViewPager);
     }
 }
